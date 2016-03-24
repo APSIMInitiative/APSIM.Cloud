3970505260408ffc609d0f5b660f6549791376ba
Dean Holzworth <Dean.Holzworth@csiro.au>
2016-03-24 14:43:27 +1000

    Missing files

---
+++ APSIM.Cloud.Runner/RunnerService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using APSIM.Cloud.Shared;
+using APSIM.Shared.Utilities;
+
+namespace APSIM.Cloud.Runner
+{
+    partial class RunnerService : ServiceBase
+    {
+        /// <summary>The job manager to send our jobs to</summary>
+        private JobManager jobManager;
+
+        public RunnerService()
+        {
+            InitializeComponent();
+            jobManager = null;
+        }
+
+        
+
+        protected override void OnStart(string[] args)
+        {
+            if (jobManager == null)
+            {
+                jobManager = new JobManager();
+                jobManager.AddJob(new RunJobsInDB());
+            }
+            jobManager.Start(waitUntilFinished: false);
+        }
+
+        protected override void OnStop()
+        {
+            jobManager.Stop();
+        }
+    }
+}
---
+++ APSIM.Cloud.Runner/RunnerServiceInstaller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+
+namespace APSIM.Cloud.Runner
+{
+    [RunInstaller(true)]
+    public partial class RunnerServiceInstaller : System.Configuration.Install.Installer
+    {
+        public RunnerServiceInstaller()
+        {
+            InitializeComponent();
+        }
+
+        private void serviceProcessInstaller1_BeforeInstall(object sender, InstallEventArgs e)
+        {
+            System.ServiceProcess.ServiceProcessInstaller lInstaller = sender as System.ServiceProcess.ServiceProcessInstaller;
+            if (lInstaller != null)
+            {
+                string lAssemblyPath = lInstaller.Context.Parameters["assemblypath"];
+                //Wrap the existing path in quotes if it isn't already
+                if (lAssemblyPath.StartsWith("\"") == false)
+                {
+                    lAssemblyPath = String.Format("\"{0}\"", lAssemblyPath);
+                }
+                lAssemblyPath += (" -Service");
+                //Set the new path
+                lInstaller.Context.Parameters["assemblypath"] = lAssemblyPath;
+            }
+        }
+    }
+}
